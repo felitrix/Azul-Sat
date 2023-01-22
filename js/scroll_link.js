dc5643f41a696728700efd9149a4775259994ca1
Felipe Koji <felipekoji@proton.me>
2023-01-22 19:43:29 -0300

    removed unused things

--- js/scroll_link.js
+++ js/scroll_link.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Adicionando rolagem suave aos links do nav
+// The code above adds smooth scrolling to the links in the nav bar. When a link is clicked, the default action of the event is prevented and then the link's href attribute is saved in the link variable. Then, the html and body elements are animated so that the page scrolls to the top of the element that the link is pointing to. The animation is set to take 850 milliseconds.
   $("nav a").on("click", function (event) {
     event.preventDefault();
     var link = $(this).attr("href");
@@ -11,4 +11,4 @@ $(document).ready(function () {
     );
   });
 });
-p;
+;
